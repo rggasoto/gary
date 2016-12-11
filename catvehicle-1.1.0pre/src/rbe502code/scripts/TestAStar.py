9ff652ed1604adfe007f874c92a58045a8d30dd5
Renato Girardi Gasoto <re.gasoto@gmail.com>
2016-12-10 21:08:23 -0500

    Revert "Add Nurburgring speed track"
    This reverts commit e062afca06899305a0f678232a4db9a7ac8beaad.

--- catvehicle-1.1.0pre/src/rbe502code/scripts/TestAStar.py
+++ catvehicle-1.1.0pre/src/rbe502code/scripts/TestAStar.py
@@ -7,21 +7,19 @@ from math import *
 #define car transform
 
 def scale(xy):
-    return xy*2.5 #each pixel is 1/4 of a meter
-imgpath = "Path4.jpg"
+    return xy*20 #each pixel is 1/4 of a meter
+imgpath = "path2.png"
 carSize = scale(np.array([2,5])) # Car is 2x5 meters in average
 road = scipy.misc.imread(imgpath,flatten=True)
-road = (road <= 64).astype(int)#treshold path
+road = (road == 0).astype(int)#treshold path
 
 # start = state(92.5,3.75,-pi/2,0,1)
 # goal = state(92.5,3.75,-pi,0,1)
-#start = state(2.5,16.75,pi/2,0)
+start = state(2.5,16.75,pi/2,0)
 #goal = state(5.5,20.75,-pi/2,0)
-#goal = state(90.5,91,pi,0)
+goal = state(90.5,91,pi,0)
 #
-start = state(322/2.5,654/2.5,2*pi/8,0)
-#goal = state(714/2.5,420/2.5,0,0)
-goal = state(695/2.5,391/2.5,-3*pi/8,0)
+
 def getCarShape(carPos):
     pixelxy = scale(np.array([carPos.x,carPos.y]));
     carRect1 =  np.array([- carSize[0]/2,- carSize[1]/2-scale(1.5)])
@@ -75,12 +73,12 @@ print collisionCheck(goal)
 print collisionCheck(start)
 
 # stepsize,heuristics,v, collisionCheck,maxBranch,steeringSpeed,steeringLimit,L)
-finder = Astar(.25,"dubin",6,collisionCheck,5,radians(100),radians(35),4.5)
+finder = Astar(.25,"euclidean",6,collisionCheck,5,radians(100),radians(35),4.5)
 path = finder.search(start,goal);
 
 for i in finder.c_visited:
-    draw.ellipse([scale(i.x)-5,scale(i.y)-5,scale(i.x)+5,scale(i.y)+5],fill=(0,255,0,0))
-    draw.polygon(matrix2Tuples(getCarShape(i)),fill=(0,255,255,0),outline = (0,0,255,250))
+    draw.ellipse([scale(i.x)-5,scale(i.y)-5,scale(i.x)+5,scale(i.y)+5],fill=(0,255,0,100))
+    draw.polygon(matrix2Tuples(getCarShape(i)),fill=(0,255,255,0),outline = (255,0,0,150))
 draw.polygon(matrix2Tuples(getCarShape(start)),fill=(255,0,0))
 if path:
     rgb = np.zeros((4,len(path)),dtype = np.int)
@@ -106,8 +104,8 @@ plotxy = []
 for i in range(len(xy)):
     plotxy.append((p[i,0],p[i,1]))
 print plotxy
-#draw.line(plotxy,fill=(255,255,255,255),width=3)
-#draw.point(plotxy[0],fill=(0,255,0,255))
+draw.line(plotxy,fill=(255,255,255,255),width=3)
+draw.point(plotxy[0],fill=(0,255,0,255))
 
 q0 = (finder.closest.x,finder.closest.y,finder.closest.theta-pi/2)
 draw.ellipse([scale(q0[0])-5,scale(q0[1])-5,scale(q0[0])+5,scale(q0[1])+5],fill=(0,255,0,100))
